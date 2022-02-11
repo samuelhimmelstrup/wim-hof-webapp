00548665849774e68cfb4f0379ba0b53579c6d9b
samuelhimmelstrup <samuelhimmelstrup@gmail.com>
2022-02-11 17:35:11 +0100

    Worked on animation + alt landing page (TTI)

--- components/BreathingSession.js
+++ components/BreathingSession.js
@@ -1,19 +1,51 @@
 import styles from "./BreathingSession.module.css"
-// import useSound from "use-sound"
-// import breathSound from "../public/sighBreath.mp3"
+import { useState } from "react"
+import dynamic from 'next/dynamic'
+const Anime = dynamic(() => import('react-anime'), {ssr: false})
+
+import { motion } from "framer-motion"
+
 
 function BreathingSession(props) {
 
+    const sessionData = {
+        rounds: 3, 
+        breaths: 4,
+        holds: 30
+    }
 
-    // const { rounds, breaths, holds } = props.SessionData
+    const { rounds, breaths, holds } = sessionData
 
-
+    let [count, setCount] = useState(0);
 
     return ( 
         <div className={styles.container}>
-            <h1>HER ER DIN SESSION</h1> 
+            <h1 className={styles.title}>HER ER DIN SESSION</h1> 
 
-            
+            {/* <div className={styles.breathBubbleContainer}>
+ 
+                <Anime easing="easeInOutSine"
+                duration={2000}
+                direction="alternate"
+                loop={breaths}
+                scale={[1, 5]}
+                complete={() => {setCount(count + 1)}}
+                >
+                    <div className={styles.breathBubble}>
+                        <p className={styles.breathCount}>{count}</p>
+                    </div>
+                </Anime>
+            </div> */}
+
+            <motion.circle 
+            animate={{ 
+                scale: 5,
+                backgroundColor: "black",
+                width: "100%",
+                boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)", 
+                }} 
+            />
+
         </div>
      );
 }
@@ -21,7 +53,21 @@ function BreathingSession(props) {
 export default BreathingSession;
 
 
+{/* <Anime easing="easeInOutSine"
+duration={2000}
+direction="alternate"
+loop={breaths}
+scale={[0.2, 1]}
+rotate={100}
+>
+    <div className={styles.innerBubble} />
+    {/* {breathCounter++} */}
+// </Anime> */}
+
+
 // LYDRELATERET
+// import useSound from "use-sound"
+// import breathSound from "../public/sighBreath.mp3"
 
 // const [play] = useSound(breathSound);
 // <button onClick={play}>play sound</button> 
--- pages/expIndex.js
+++ pages/expIndex.js
@@ -1,49 +1,23 @@
-import styles from '../styles/Home.module.css'
-import { useState } from "react"
-import SessionsBox from "../components/SessionsBox"
-import Image from "next/image"
-import Backdrop from "../layout/Backdrop"
+import styles from '../styles/expIndex.module.css'
 import Footer from "../components/Footer"
+import BreathingSession from '../components/BreathingSession'
 
 export default function Home() {
 
-  const [showSessions, setShowSessions] = useState(false);
-
-  const showSessionsHandler = () => {
-    if (showSessions) {
-      setShowSessions(false)
-    }
-    else
-      setShowSessions(true)
-    }
-
   return (    
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
-        <div className={styles.titleAndBtnBox}>
-          <h1 className={styles.title}>Test</h1>
-          <button type="button" className={styles.btn} onClick={showSessionsHandler}>SESSIONS</button>
-        </div>
-        
-        <div className={styles.nightsky}>
-          <Image src="/../public/images/nightsky.jpg" layout="fill" priority="true" />
-        </div>
-
-        <div className={styles.clouds}>
-          <Image src="/../public/images/clouds.png" layout="fill" />
-        </div>
-        
-        { showSessions && <SessionsBox onClicking={showSessionsHandler}/>}   
-        { showSessions && <Backdrop onClicking={showSessionsHandler}/> } 
+    <>
+      <div className={styles.sessionContainer}>
+        <BreathingSession />
       </div>
 
-      <main className={styles.mainContent}>
+      {/* <main className={styles.mainContent}>
         <h1>Description</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adip</p>
       </main>
 
-      <Footer />
+      <Footer /> */}
+    </>
 
-    </div>
+
   )
 }
