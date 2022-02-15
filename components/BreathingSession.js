357ce41664f2e7a6dbe17ec5f6ae36cb342ad03d
samuelhimmelstrup <samuelhimmelstrup@gmail.com>
2022-02-15 09:40:01 +0100

    Options for breathType, animation repeat  fixed

--- components/BreathingSession.js
+++ components/BreathingSession.js
@@ -1,5 +1,5 @@
 import styles from "./BreathingSession.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { motion } from "framer-motion"
 import CountDown from "./CountDown"
 
@@ -8,34 +8,60 @@ function BreathingSession(props) {
 
     const sessionData = {
         rounds: 3, 
-        breaths: 2,
-        holds: 30
+        breaths: 30,
+        breathType: "quick", // ("quick", "normal") 
+        holds: 8,
+        silentHolds: false, // TODO: implement silentHolds option (& sound in general)
     }
 
-    const { rounds, breaths, holds } = sessionData
+    const { rounds, breaths, holds, breathType, silentHolds} = sessionData;
+
+    const breathLength = breathType == "slow" ? 6 
+                         : breathType == "quick" ? 2 
+                         : 4;
 
     const [isBreathing, setIsBreathing] = useState(true);
+    const [count, setCount] = useState(1);
 
     const endOfBreaths = () => {
-        console.log("SLUT PÅ VEJRTRÆKNING")
-        setIsBreathing(false)
+        console.log("SLUT PÅ VEJRTRÆKNING");
+        setIsBreathing(false);
     }
-
-
     const endOfCountDown = () => {
-        console.log("TID TIL NÆSTE RUNDE")
+        setIsBreathing(true);
+        console.log("TID TIL NÆSTE RUNDE");
     }
 
     const bubbleVariants = {
         animationOne: {
             scale: [1, 3],
-            backgroundColor: "#982132e1",
+            backgroundColor: "#982132",
             transition: {
-                duration: 2,
-                yoyo: breaths // PRØV MED REPEAT
+                duration: breathLength / 2,
+                repeat: breaths + 1,
+                repeatType: "reverse"
+            }
+        }, 
+        animationEnd: {
+            scale: [1, 6],
+            backgroundColor: ["#982132" , "#202132"],
+            transition: {
+                duration: 4, 
+                easing: "easeInOut",
+                repeat: Infinity,
+                repeatType: "reverse"
             }
         }
     }
+
+    // INCREASES COUNTER EVERY 4 SECONDS (every breath)
+    // TODO: Only increases once??
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setCount(count + 1);
+        }, breathLength*1000);
+        return () => clearInterval(interval);
+      }, []);
 
     return (        
 
@@ -45,14 +71,14 @@ function BreathingSession(props) {
             <div className={styles.breathBubbleContainer}>
                 <motion.div className={styles.breathBubble}
                 variants={bubbleVariants}
-                animate="animationOne" 
+                animate={isBreathing? "animationOne" : "animationEnd"}
                 >
-                <p className={styles.counter}>TÆLLER HER</p>
+                <p className={styles.counter}>{isBreathing ? count : "Hold breath"}</p>
                 </motion.div>
             </div>
 
             <div className={styles.hidden}>
-                <CountDown time={breaths*2} onComplete={endOfBreaths} />
+                <CountDown time={breaths*breathLength} onComplete={endOfBreaths} />
             </div>
 
 
